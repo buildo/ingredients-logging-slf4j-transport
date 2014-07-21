46a1d580169d563a285859866ee9e806d26dbd6a
Gabriele Petronella <gabriele@buildo.io>
2014-07-22 01:11:14 +0200

    Factorize underlying logger call

--- src/main/scala/io.buildo.ingredients/logging/transport/SLF4JLogger.scala
+++ src/main/scala/io.buildo.ingredients/logging/transport/SLF4JLogger.scala
@@ -5,41 +5,33 @@ import io.buildo.ingredients.logging._
 import org.slf4j.{ LoggerFactory, Logger => Underlying }
 
 class SLF4J extends Transport {
+
   private val loggers = scala.collection.mutable.HashMap[String, Underlying]()
 
   def write(name: String, msg: LogMessage): Unit = {
+
     val underlying = loggers.get(name) getOrElse {
       var l = LoggerFactory.getLogger(name)
       loggers += (name -> l)
       l
     }
+
     val c = msg.cause
     val m = s"[${msg.fileName}:${msg.line}] ${msg.message}"
+
+    def callUnderlying(
+      withCause: (String, Throwable) => Unit,
+      withoutCause: String => Unit
+    ) = c match {
+      case Some(e) => withCause(m, e)
+      case None => withoutCause(m)
+    }
+
     msg.level match {
-      case Level.Debug => {
-        if (c.isDefined)
-          underlying.debug(m, c.get)
-        else
-          underlying.debug(m)
-      }
-      case Level.Info => {
-        if (c.isDefined)
-          underlying.info(m, c.get)
-        else
-          underlying.info(m)
-      }
-      case Level.Warn => {
-        if (c.isDefined)
-          underlying.warn(m, c.get)
-        else
-          underlying.warn(m)
-      }
-      case Level.Error => {
-        if (c.isDefined)
-          underlying.error(m, c.get)
-        else
-          underlying.error(m)
-      }
+      case Level.Debug => callUnderlying(underlying.debug, underlying.debug)
+      case Level.Info => callUnderlying(underlying.info, underlying.info)
+      case Level.Warn => callUnderlying(underlying.warn, underlying.warn)
+      case Level.Error => callUnderlying(underlying.error, underlying.error)
     }
   }
 }
